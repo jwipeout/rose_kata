ee5f5861607f7bac9bdb454fa0979408f40740af
Josh Cavin <jcavin9@gmail.com>
2019-12-12 07:24:09 -0800

    Made mutation of instance variable more idiomatic

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,50 +10,46 @@ class GildedRose
   def tick
     if @name == "Aged Brie"
       if @quality < 50
-        @quality = @quality + 1
+        @quality += 1
       end
 
-      @days_remaining = @days_remaining - 1
+      @days_remaining -= 1
 
       if @days_remaining < 0
         if @quality < 50
-          @quality = @quality + 1
+          @quality += 1
         end
       end
     elsif @name == "Backstage passes to a TAFKAL80ETC concert"
       if @quality < 50
 
-        @quality = @quality + 1
+        @quality += 1
 
         if @days_remaining < 11
-          if @quality < 50
-            @quality = @quality + 1
-          end
+          @quality += 1
         end
 
         if @days_remaining < 6
-          if @quality < 50
-            @quality = @quality + 1
-          end
+          @quality += 1
         end
       end
 
-      @days_remaining = @days_remaining - 1
+      @days_remaining -= 1
 
       if @days_remaining < 0
-        @quality = @quality - @quality
+        @quality -= @quality
       end
     elsif @name == "Sulfuras, Hand of Ragnaros"
     else
       if @quality > 0
-        @quality = @quality - 1
+        @quality -= 1
       end
 
-      @days_remaining = @days_remaining - 1
+      @days_remaining -= 1
 
       if @days_remaining < 0
         if @quality > 0
-          @quality = @quality - 1
+          @quality -= 1
         end
       end
     end

59b08b9d32410481d4511e49b0e1728b9dd3a06e
Josh Cavin <jcavin9@gmail.com>
2019-12-12 06:24:46 -0800

    changed logic from not to equal

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,13 +8,7 @@ class GildedRose
   end
 
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
+    if @name == "Aged Brie"
       if @quality < 50
         @quality = @quality + 1
         if @name == "Backstage passes to a TAFKAL80ETC concert"
@@ -30,24 +24,51 @@ class GildedRose
           end
         end
       end
+    elsif @name == "Backstage passes to a TAFKAL80ETC concert"
+      if @quality < 50
+        @quality = @quality + 1
+        if @name == "Backstage passes to a TAFKAL80ETC concert"
+          if @days_remaining < 11
+            if @quality < 50
+              @quality = @quality + 1
+            end
+          end
+          if @days_remaining < 6
+            if @quality < 50
+              @quality = @quality + 1
+            end
+          end
+        end
+      end
+    else
+      if @quality > 0
+        if @name == "Sulfuras, Hand of Ragnaros"
+        else
+          @quality = @quality - 1
+        end
+      end
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
+
+    if @name == "Sulfuras, Hand of Ragnaros"
+    else
       @days_remaining = @days_remaining - 1
     end
+
     if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
+      if @name == "Aged Brie"
+        if @quality < 50
+          @quality = @quality + 1
+        end
+      else
+        if @name == "Backstage passes to a TAFKAL80ETC concert"
+          @quality = @quality - @quality
+        else
           if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
+            if @name == "Sulfuras, Hand of Ragnaros"
+            else
               @quality = @quality - 1
             end
           end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
         end
       end
     end

526d0eaf0b1b3eb17dd37f325115c55998ddec07
Josh Cavin <jcavin9@gmail.com>
2019-12-12 08:57:27 -0800

    Item factory for creating correct item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,97 @@
+class ItemFactory
+  def self.build(name)
+    if name == "Aged Brie"
+      AgedBrie.new
+    elsif name == "Backstage passes to a TAFKAL80ETC concert"
+      BackstagePass.new
+    elsif name == "Conjured Mana Cake"
+      ConjuredManaCake.new
+    elsif name == "Sulfuras, Hand of Ragnaros"
+      SulfurasHand.new
+    else
+      NormalItem.new
+    end
+  end
+end
+
+class AgedBrie
+  def tick(degrade)
+    if degrade.quality < 50
+      degrade.quality += 1
+    end
+
+    degrade.days_remaining -= 1
+
+    if degrade.days_remaining < 0
+      if degrade.quality < 50
+        degrade.quality += 1
+      end
+    end
+  end
+end
+
+class BackstagePass
+  def tick(degrade)
+    if degrade.quality < 50
+
+      degrade.quality += 1
+
+      if degrade.days_remaining < 11
+        degrade.quality += 1
+      end
+
+      if degrade.days_remaining < 6
+        degrade.quality += 1
+      end
+    end
+
+    degrade.days_remaining -= 1
+
+    if degrade.days_remaining < 0
+      degrade.quality -= degrade.quality
+    end
+  end
+end
+
+class ConjuredManaCake
+  def tick(degrade)
+    if degrade.quality > 0
+      degrade.quality -= 2
+    end
+
+    degrade.days_remaining -= 1
+
+    if degrade.days_remaining < 0
+      if degrade.quality > 0
+        degrade.quality -= 2
+      end
+    end
+  end
+end
+
+class SulfurasHand
+  def tick(degrade)
+  end
+end
+
+class NormalItem
+  def tick(degrade)
+    if degrade.quality > 0
+      degrade.quality -= 1
+    end
+
+    degrade.days_remaining -= 1
+
+    if degrade.days_remaining < 0
+      if degrade.quality > 0
+        degrade.quality -= 1
+      end
+    end
+  end
+end
+
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
+  attr_accessor :name, :days_remaining, :quality
 
   def initialize(name:, days_remaining:, quality:)
     @name = name
@@ -9,61 +101,15 @@ class GildedRose
 
   def tick
     if @name == "Aged Brie"
-      if @quality < 50
-        @quality += 1
-      end
-
-      @days_remaining -= 1
-
-      if @days_remaining < 0
-        if @quality < 50
-          @quality += 1
-        end
-      end
+      ItemFactory.build(name).tick(self)
     elsif @name == "Backstage passes to a TAFKAL80ETC concert"
-      if @quality < 50
-
-        @quality += 1
-
-        if @days_remaining < 11
-          @quality += 1
-        end
-
-        if @days_remaining < 6
-          @quality += 1
-        end
-      end
-
-      @days_remaining -= 1
-
-      if @days_remaining < 0
-        @quality -= @quality
-      end
+      ItemFactory.build(name).tick(self)
     elsif @name == "Conjured Mana Cake"
-      if @quality > 0
-        @quality -= 2
-      end
-
-      @days_remaining -= 1
-
-      if @days_remaining < 0
-        if @quality > 0
-          @quality -= 2
-        end
-      end
+      ItemFactory.build(name).tick(self)
     elsif @name == "Sulfuras, Hand of Ragnaros"
+      ItemFactory.build(name).tick(self)
     else
-      if @quality > 0
-        @quality -= 1
-      end
-
-      @days_remaining -= 1
-
-      if @days_remaining < 0
-        if @quality > 0
-          @quality -= 1
-        end
-      end
+      ItemFactory.build(name).tick(self)
     end
   end
 end
